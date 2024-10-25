bc9cec7439b1e2f0b7165f2123b72582e58ea19d
Rodolfo Lopez Lozano <201606416@gmail.com>
2024-10-24 22:58:39 -0400

    [UPDATE] error in merge

--- src/screen/ShoppingScreen/ShoppingScreen.jsx
+++ src/screen/ShoppingScreen/ShoppingScreen.jsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import { ListCardComponent } from '../../components/listCard.component';
-import { useDialog } from '../../Stores/global.store';
-import { TrashComponent } from '../../components/trash.component';
 
 export const ShoppingScreen = () => {
-	const isVisible = useDialog((state) => state.isVisible);
-	const hideDialog = useDialog((state) => state.hideDialog);
 	return (
 		<View style={{ padding: 15 }}>
 			<ListCardComponent />
-			{isVisible && <TrashComponent isVisible={isVisible} hideDialog={hideDialog}/>}
 		</View>
 	);
 };
