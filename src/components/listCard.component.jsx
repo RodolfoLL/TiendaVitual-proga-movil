7532b56e0677bf092496a84a37a4af1952e5aec1
Rodolfo Lopez Lozano <201606416@gmail.com>
2024-11-01 14:33:08 -0400

    [UPDATE] boton menos de la listCard no elimina el producto

--- src/components/listCard.component.jsx
+++ src/components/listCard.component.jsx
@@ -3,68 +3,61 @@ import { Button, Card, Text } from 'react-native-paper';
 import { useCartStore } from '../Stores/card.store';
 
 export const ListCardComponent = () => {
-  const productSelected = useCartStore((state) => state.cartItems);
-  const addToCart = useCartStore((state) => state.addToCart);
-  const removeFromCart = useCartStore((state) => state.removeFromCart);
-  const setQuantity = useCartStore((state) => state.setQuantity);
+	const productSelected = useCartStore((state) => state.cartItems);
+	const addToCart = useCartStore((state) => state.addToCart);
+	const removeFromCart = useCartStore((state) => state.removeFromCart);
+	const setQuantity = useCartStore((state) => state.setQuantity);
 
-  const increaseQuantity = (product) => {
-    addToCart(product);
-  };
+	const increaseQuantity = (product) => {
+		addToCart(product);
+	};
 
-  const decreaseQuantity = (product) => {
-    if (product.cantidad > 1) {
-      setQuantity(product.producto_id, product.cantidad - 1);
-    } else {
-      removeFromCart(product.producto_id);
-    }
-  };
+	const decreaseQuantity = (product) => {
+		if (product.cantidad > 1) {
+			setQuantity(product.producto_id, product.cantidad - 1);
+		}
+	};
 
-  const deleteProduct = (productId) => {
-    removeFromCart(productId);
-  };
+	const deleteProduct = (productId) => {
+		removeFromCart(productId);
+	};
 
-  return (
-    <View style={{ marginBottom: 10 }}>
-      {productSelected.map((element) => (
-        <Card key={element.producto_id} style={{ marginBottom: 10 }}>
-          <Card.Title
-            title={element.nombre_producto}
-            subtitle={`Precio: ${element.precio}`}
-            style={{ paddingTop: 20 }}
-            titleVariant='titleLarge'
-          />
-          <Card.Actions>
-            <Button
-              icon='plus'
-              mode='contained'
-              labelStyle={{ marginLeft: 10 }}
-              onPress={() => increaseQuantity(element)}
-              style={{backgroundColor:"#9C7CFE"}}
-
-            >
-            </Button>
-            <Text variant='headlineSmall'>{element.cantidad}</Text>
-            <Button
-              icon='minus'
-              mode='contained'
-              labelStyle={{ marginLeft: 10 }}
-              onPress={() => decreaseQuantity(element)}
-              style={{backgroundColor:"#9C7CFE"}}
-
-            >
-            </Button>
-            <Button
-              icon='delete'
-              mode='contained'
-              labelStyle={{ marginLeft: 10 }}
-              onPress={() => deleteProduct(element.producto_id)}
-              style={{backgroundColor:"#9C7CFE"}}
-            >
-            </Button>
-          </Card.Actions>
-        </Card>
-      ))}
-    </View>
-  );
+	return (
+		<View style={{ marginBottom: 10 }}>
+			{productSelected.map((element) => (
+				<Card key={element.producto_id} style={{ marginBottom: 10 }}>
+					<Card.Title
+						title={element.nombre_producto}
+						subtitle={`Precio: ${element.precio}`}
+						style={{ paddingTop: 20 }}
+						titleVariant='titleLarge'
+					/>
+					<Card.Actions>
+						<Button
+							icon='plus'
+							mode='contained'
+							labelStyle={{ marginLeft: 10 }}
+							onPress={() => increaseQuantity(element)}
+							style={{ backgroundColor: '#9C7CFE' }}
+						></Button>
+						<Text variant='headlineSmall'>{element.cantidad}</Text>
+						<Button
+							icon='minus'
+							mode='contained'
+							labelStyle={{ marginLeft: 10 }}
+							onPress={() => decreaseQuantity(element)}
+							style={{ backgroundColor: '#9C7CFE' }}
+						></Button>
+						<Button
+							icon='delete'
+							mode='contained'
+							labelStyle={{ marginLeft: 10 }}
+							onPress={() => deleteProduct(element.producto_id)}
+							style={{ backgroundColor: '#9C7CFE' }}
+						></Button>
+					</Card.Actions>
+				</Card>
+			))}
+		</View>
+	);
 };
