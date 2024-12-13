bb0d97f7015bbe0ed5b94be5ed7aabf68c25bd16
Rodolfo Lopez Lozano <201606416@gmail.com>
2024-12-13 17:08:17 -0400

    [UPDATE] se hace la busqueda segun una direccion

--- src/screen/DeliveryMap/DeliveryMapComponent.jsx
+++ src/screen/DeliveryMap/DeliveryMapComponent.jsx
@@ -9,29 +9,68 @@ import {
 import MapView, { Marker, Polyline } from 'react-native-maps';
 import * as Location from 'expo-location';
 import MapViewDirections from 'react-native-maps-directions';
+import axios from 'axios';
+import { LogBox } from 'react-native';
 import { GOOGLE_MAPS_KEY } from '@env';
 
 export const DeliveryMapComponent = () => {
+	const addressTest = 'calle calama esquina san martin';
+	LogBox.ignoreLogs([
+		'MapViewDirections Error: Error on GMAPS route request: NOT_FOUND',
+		'Directions error: Error on GMAPS route request: NOT_FOUND',
+	  ]);
+	const [myDestination, setmyDestination] = useState({
+		latitude: 0,
+		longitude: 0,
+	});
 	const [origin, setorigin] = useState({
 		latitude: -17.393862717524335, // Ejemplo con coordenadas de la umss
 		longitude: -66.14728339607203,
 	});
-	const [destination, setdestination] = useState({
-		latitude: -17.394862717524335, // Ubicación cercana a la umss
-		longitude: -66.14828339607203,
-	});
-
-	const [currentLocation, setCurrentLocation] = useState(null);
 	const [initialRegion, setInitialRegion] = useState({
 		latitude: origin.latitude,
 		longitude: origin.longitude,
 		latitudeDelta: 0.1,
 		longitudeDelta: 0.1,
 	});
-  
+	const geocodeAddress = async (address) => {
+		try {
+			const response = await axios.get(
+				`https://maps.googleapis.com/maps/api/geocode/json`,
+				{
+					params: {
+						address: address,
+						region: 'BO', // Restringe la búsqueda a Bolivia
+						key: GOOGLE_MAPS_KEY,
+						bounds: '-17.50,-66.30|-17.20,-65.80',
+					},
+				}
+			);
+
+			if (response.data.status === 'OK' && response.data.results.length > 0) {
+				const location = response.data.results[0].geometry.location;
+				console.log('Geocoded location:', location);
+				setmyDestination({
+					latitude:location['lat'],
+					longitude:location['lng']
+				})
+				return location; // { latitude, longitude }
+			} else {
+				console.warn('No results found for the address');
+				return null;
+			}
+		} catch (error) {
+			console.error('Error fetching geocoding data:', error);
+			return null;
+		}
+	};
 	useEffect(() => {
-		getCurrentLocation();
-	}, []);
+		if (addressTest.length > 0) {
+			geocodeAddress(addressTest);
+		} else {
+			getCurrentLocation();
+		}
+	}, [addressTest]);
 	async function getCurrentLocation() {
 		let { status } = await Location.requestForegroundPermissionsAsync();
 		if (status !== 'granted') {
@@ -40,12 +79,10 @@ export const DeliveryMapComponent = () => {
 		}
 
 		let location = await Location.getCurrentPositionAsync({});
-		setCurrentLocation(location);
-		const current = {
+		setmyDestination({
 			latitude: location.coords.latitude,
 			longitude: location.coords.longitude,
-		};
-		setdestination(current);
+		});
 	}
 
 	return (
@@ -66,7 +103,7 @@ export const DeliveryMapComponent = () => {
 
 				{/* Delivery Destination Marker */}
 				<Marker
-					coordinate={destination}
+					coordinate={myDestination}
 					title='Destino'
 					description='Ubicación de entrega'
 					pinColor='red'
@@ -85,10 +122,11 @@ export const DeliveryMapComponent = () => {
         )} */}
 				<MapViewDirections
 					origin={origin}
-					destination={destination}
+					destination={myDestination}
 					apikey={GOOGLE_MAPS_KEY}
 					strokeColor='#9C7CFE'
 					strokeWidth={6}
+					onError={(errorMessage) => console.warn('Directions error:', errorMessage)}
 				/>
 			</MapView>
 			<Text style={styles.infoText}>Tu pedido está en camino.</Text>
